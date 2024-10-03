62dd73864cbcfb9bcf29751910b2e1ea4ef130e6
Mariana Oliveira Braga De Morais <mariaoli@c1r9s2.42porto.com>
2024-10-03 19:13:55 +0100

    Added system libraries to minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,7 +1,27 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+// system libraries
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
+// our own libraries
+# include "../libft/libft.h"
 
 // main.c
 int	main(void);
