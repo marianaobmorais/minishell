c6ef66122e2fa8af2247cb9406a34480e48afa30
joseevilasio <jose_evilasio@outlook.com>
2024-10-13 19:27:11 +0100

    added ft export

--- includes/builtins.h
+++ includes/builtins.h
@@ -2,5 +2,6 @@
 #define BUILTINS_H
 
 int	ft_pwd(void);
+int	ft_export(char *str);
 
 #endif
---
+++ srcs/builtins/ft_export.c
@@ -0,0 +1,48 @@
+#include "../../includes/minishell.h"
+
+extern char **environ;
+
+static int	replace_env(char *str, size_t size)
+{
+	int	i;
+
+	i = 0;
+	while(environ[i])
+	{
+		if (ft_strncmp(str, environ[i], size) == 0)
+		{
+			free(environ[i]);
+			environ[i] = str;
+			return (0);
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	ft_export(char *str)
+{
+	size_t	s_key;
+	size_t	i;
+	char	**new_environ;
+
+	if (!str || !ft_strchr(str, '='))
+		return (-1);
+	s_key = (ft_strlen(str) - ft_strlen(ft_strchr(str, '='))) + 1;
+	i = replace_env(str, s_key);
+	if (i == 0)
+		return (0);
+	new_environ = (char **) malloc((i + 2) * sizeof(char *));
+	if (!new_environ)
+		return (-1);
+	i = 0;
+	while(environ[i])
+	{
+		new_environ[i] = environ[i];
+		i++;
+	}
+	environ = new_environ;
+	environ[i] = str;
+	environ[i + 1] = NULL;
+	return (0);
+}
