1a5a441bd81c46804f4d0a593691cc1f3b228ff6
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2024-10-15 16:13:48 +0100

    Added ft_env.c

---
+++ srcs/builtins/ft_env.c
@@ -0,0 +1,22 @@
+#include "../../includes/minishell.h"
+
+void ft_env(char **my_envp)
+{
+	int	i;
+
+	i = 0;
+	while (my_envp[i])
+	{
+		printf("%s\n", my_envp[i]);
+		i++;
+	}
+}
+
+/* int main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+
+	ft_env(envp);
+	return (0);
+} */
