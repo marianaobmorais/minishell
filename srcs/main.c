a9408ad3c6879c15bae59d4ffcf1be0adeabea04
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2024-10-22 18:38:02 +0100

    main.c has tests for cd, echo and env

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,91 @@
 #include "../includes/minishell.h"
 
-int	main(void)
+void	ft_free_vector(char **vector)
 {
-	printf("Oi, eu sou o bashinho\n");
+	int	i;
+
+	i = 0;
+	if (vector)
+	{
+		while (vector[i])
+		{
+			free(vector[i]);
+			i++;
+		}
+	}
+	free(vector);
+}
+//para testar o echo
+/* int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	ft_echo("-nnnnnnnnnnnnnnnnnnnn -nn -no -n oi   'oi'oi'' \"tchau\"");
+	return (0);
+} */
+
+
+int	main(int argc, char **envp)
+{
+	char	**my_envp;
+	char	*input;
+
+	if (argc != 1)
+	{
+		printf("minishell: arguments are not accepted when initializing the program\n"); //ft_error_handler();
+		return (1); // conferir numero de saida
+	}
+	my_envp = ft_get_my_envp(envp);
+	if (!my_envp)
+	{
+		ft_error_handler();
+		return (1); // conferir numero de saida
+	}
+	while (1)
+	{
+		input = readline(PROMPT);
+		printf("esse eh o input: %s\n", input);
+	}
+	ft_free_vector(my_envp);
 	return (0);
 }
+
+
+//pra testar cd
+/* int	main(int argc, char **argv, char **envp)
+{
+	(void)argv;
+	char **my_envp;
+
+	// if (argc != 1)
+	// {
+	// 	ft_error_handler();
+	// 	return (1); // conferir numero de saida
+	// }
+	my_envp = ft_get_my_envp(envp);
+	if (!my_envp)
+		return (1);
+
+	int	i = 0;
+	while (my_envp[i])
+	{
+		printf("%s\n", my_envp[i]);
+		i++;
+	}
+
+	ft_cd(argc, argv[1], my_envp);
+
+	printf("\n\n");
+	i = 0;
+	while (my_envp[i])
+	{
+		printf("%s\n", my_envp[i]);
+		i++;
+	}
+
+	ft_free_vector(my_envp);
+	return (0);
+} */
+
+
