f200992c3c9d9d404a9f5b3f30f407ae61cbb76f
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2024-10-22 18:34:07 +0100

    ft_echo now accepts -n flag

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -1,25 +1,47 @@
 #include "../../includes/minishell.h"
 
+bool	ft_arg_is_flag(char *s)
+{
+	int	i;
+
+	if (!(s[0] == '-' && s[1] == 'n')) // quais são as outras flags? aceitar como string ou mandar mensagem de erro?
+		return (false);
+	else
+	{
+		i = 2;
+		while (s[i] == 'n')
+			i++;
+		if (s[i] == '\0')
+			return (true);
+		return (false);
+	}
+}
+
 void	ft_echo(char *args)
 {
-	char	**str;
+	char	**s;
 	bool	flag;
+	int		i;
 
 	flag = false;
-	//se tiver flag -n? -nnnn? -n -nnn -n?
 	if (args == NULL) // sem argumento
 	{
 		printf("\n");
 		return ;
 	}
-	str = ft_split_quotes(args, 32);
-	if (!str)
+	s = ft_split_quotes(args, ' ');
+	if (!s)
 		return ;
-	int i = 0;
-	while (str[i])
+	i = 0;
+	while (ft_arg_is_flag(s[i])) // &flag will work?
 	{
-		printf("%s", str[i]);
-		if (str[i + 1])
+		flag = true;
+		i++;
+	}
+	while (s[i])
+	{
+		printf("%s", s[i]);
+		if (s[i + 1])
 			printf(" ");
 		i++;
 	}
