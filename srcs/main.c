684654ed58a2e8d93c02be5f375b17fa3324bcc9
Mariana Oliveira Braga De Morais <mariaoli@c1r15s2.42porto.com>
2024-10-29 17:45:01 +0000

    Working on parsing the input

--- srcs/main.c
+++ srcs/main.c
@@ -15,20 +15,30 @@ void	ft_free_vector(char **vector)
 	}
 	free(vector);
 }
-//para testar o echo
-/* int	main(int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
 
-	ft_echo("-nnnnnnnnnnnnnnnnnnnn -nn -no -n oi   'oi'oi'' \"tchau\"");
-	return (0);
+/* char	*ft_tokenize_input(char *input)
+{
+	char	*token;
+	t_token	*node;
+
+	while (*input)
+	{
+		if (ft_strchr(METACHARS, *input))
+		{
+			node = (t_token *)ft_lstnew(token);
+			printf("token = %s\n", token);
+		}
+		token = ft_charjoin(token, *input);
+		input++;
+	}
+	return (token);
 } */
 
-
-int	main(int argc, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
+	(void)argv;
 	char	**my_envp;
+	char	**args;
 	char	*input;
 
 	if (argc != 1)
@@ -45,47 +55,15 @@ int	main(int argc, char **envp)
 	while (1)
 	{
 		input = readline(PROMPT);
-		printf("esse eh o input: %s\n", input);
+		if (input)
+			args = ft_split_expand(input, ' ', my_envp);
+		int i = 0;
+		while (args[i])
+		{
+			printf("token %d: %s\n", i + 1, args[i]);
+			i++;
+		}
 	}
 	ft_free_vector(my_envp);
 	return (0);
 }
-
-
-//pra testar cd
-/* int	main(int argc, char **argv, char **envp)
-{
-	(void)argv;
-	char **my_envp;
-
-	// if (argc != 1)
-	// {
-	// 	ft_error_handler();
-	// 	return (1); // conferir numero de saida
-	// }
-	my_envp = ft_get_my_envp(envp);
-	if (!my_envp)
-		return (1);
-
-	int	i = 0;
-	while (my_envp[i])
-	{
-		printf("%s\n", my_envp[i]);
-		i++;
-	}
-
-	ft_cd(argc, argv[1], my_envp);
-
-	printf("\n\n");
-	i = 0;
-	while (my_envp[i])
-	{
-		printf("%s\n", my_envp[i]);
-		i++;
-	}
-
-	ft_free_vector(my_envp);
-	return (0);
-} */
-
-
