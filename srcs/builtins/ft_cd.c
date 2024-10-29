c3ed8d70cb3f2d90d7f8b6957f452ff21c93cd15
Mariana Oliveira Braga De Morais <mariaoli@c1r15s2.42porto.com>
2024-10-29 17:41:41 +0000

    Added commented main to ft_cd function

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -77,4 +77,37 @@ void	ft_cd(int argc, const char *new_dir, char **my_envp)
 	ft_update_my_envp(my_envp, cur_pwd);
 }
 
+/* int	main(int argc, char **argv, char **envp)
+{
+	(void)argv;
+	char **my_envp;
 
+	// if (argc != 1)
+	// {
+	// 	ft_error_handler();
+	// 	return (1); // conferir numero de saida
+	// }
+	my_envp = ft_get_my_envp(envp);
+	if (!my_envp)
+		return (1);
+
+	int	i = 0;
+	while (my_envp[i])
+	{
+		printf("%s\n", my_envp[i]);
+		i++;
+	}
+
+	ft_cd(argc, argv[1], my_envp);
+
+	printf("\n\n");
+	i = 0;
+	while (my_envp[i])
+	{
+		printf("%s\n", my_envp[i]);
+		i++;
+	}
+
+	ft_free_vector(my_envp);
+	return (0);
+} */
