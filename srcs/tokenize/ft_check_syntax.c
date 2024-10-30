ab8b7062b98522357d232fbd049bc05d4c2f40f5
Mariana Oliveira Braga De Morais <mariaoli@c2r2s5.42porto.com>
2024-10-30 19:13:58 +0000

    Working on check syntax

--- srcs/tokenize/ft_check_syntax.c
+++ srcs/tokenize/ft_check_syntax.c
@@ -15,22 +15,23 @@ int	ft_check_syntax(char *s)
 	bool	specialchar;
 
 	trim = ft_strtrim(s, ISSPACE);
-	
+	if (!trim)
+		return (0); //mensagem?
 	specialchar = false;
 	i = 0;
-	if (ft_strchr(SPECIALCHARS, s[i]))
+	if (ft_strchr(SPECIALCHARS, trim[i]))
 	{
-		if (s[i] != '<' && s[i] != '>')
+		if (trim[i] != '<' && trim[i] != '>')
 		{
-			printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, s[i]);
-			return (0); // add message
+			printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, trim[i]);
+			return (free(trim), 0);
 		}
 	}
-	while (s[i])
+	while (trim[i])
 	{
-		if (s[i] == 39 || s[i] == 34)
+		if (trim[i] == 39 || trim[i] == 34)
 		{
-			i = ft_next_quote(s, i, s[i]);
+			i = ft_next_quote(s, i, trim[i]);
 			if (i == -1)
 			{
 				printf("%s: open quotes are not supported\n", PROG_NAME); //ft_error_handler();
@@ -38,30 +39,39 @@ int	ft_check_syntax(char *s)
 			}
 			i++;
 			specialchar = false;
+			if (trim[i] == '\0') // check this
+				break ;
 		}
-		if (ft_strchr(SPECIALCHARS, s[i]))
+		if (ft_strchr(SPECIALCHARS, trim[i])) //check this with this
 		{
 			if (specialchar == true)
 			{
-				printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, s[i]);
+				printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, trim[i]);
+				free(trim);
 				return (0);
 			}
-			if (s[i] == '<' && s[i + 1] == s[i] && specialchar == false)
+			if (trim[i] == '<' && trim[i + 1] == trim[i] && specialchar == false)
 				i += 2;
-			else if (s[i] == '>' && s[i + 1] == s[i] && specialchar == false)
+			else if (trim[i] == '>' && trim[i + 1] == trim[i] && specialchar == false)
 				i += 2;
 			specialchar = true;
+			if (trim[i] == '\0')
+				break ;
 			i++;
 		}
-		else if (!ft_isspace(s[i]))
+		else if (!ft_isspace(trim[i]))
 		{
 			specialchar = false;
 			i++;
 		}
-		if (ft_isspace(s[i]))
+		if (ft_isspace(trim[i]))
 			i++;
 	}
-	//if (specialchar == true)
-		//printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, s[i - 1]);
+	if (specialchar == true)
+	{
+		printf("%s: syntax error near unexpected token `%c'\n", PROG_NAME, s[i + 1]);
+		return (free(trim), 0);
+	}
+	free(trim);
 	return (1);
 }
