49bb789762008774d098e6da679ed2aadde43e08
Mariana <mariana.obm@gmail.com>
2024-11-03 00:12:24 +0000

    Added function to header and file to Makefike

--- includes/tokenize.h
+++ includes/tokenize.h
@@ -4,6 +4,8 @@
 # define SPECIALCHARS "&;()\\{}[^%.`*"
 # define METACHARS "|<>"
 # define ISSPACE " \t\n\v\f\r"
+# define SQUOTE 39
+# define DQUOTE 34
 
 typedef enum e_type
 {
@@ -13,15 +15,15 @@ typedef enum e_type
 	INFILE,
 	OUTFILE,
 	APPEND,
-	HEREDOC
+	HEREDOC,
+	EXPORT
 }	t_type;
 
 typedef enum e_state
 {
 	GENERAL,
 	IN_SQUOTE,
-	IN_DQUOTE,
-	EXPORT //
+	IN_DQUOTE
 }	t_state;
 
 typedef struct s_token
@@ -43,5 +45,8 @@ int		ft_next_quote(char *s, int i, char c);
 int		ft_isspace(int c);
 void	ft_process_input(char *input, char **my_envp);
 char	*ft_charjoin(char *str, char c);
+t_list	**ft_create_token_list(char *s);
+t_type	ft_get_type(char *s);
+t_state	ft_get_state(char *s);
 
 #endif
