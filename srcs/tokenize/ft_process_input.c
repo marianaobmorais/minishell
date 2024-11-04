73dd1f2c9b5111550b2070c7abf3bdfcc86521f6
Mariana Oliveira Braga De Morais <mariaoli@c2r1s6.42porto.com>
2024-11-04 19:10:32 +0000

    Added ft_free_list

--- srcs/tokenize/ft_process_input.c
+++ srcs/tokenize/ft_process_input.c
@@ -1,7 +1,7 @@
 #include "../../includes/minishell.h"
 
 
-void	print_list(t_list **token_list)
+void	ft_print_list(t_list **token_list)
 {
 	t_list	*current;
 	t_token	*token;
@@ -11,26 +11,28 @@ void	print_list(t_list **token_list)
 		printf("List is empty\n");
 		return ;
 	}
-	printf("-----------------------------------------\n");
-	printf("| %-13s | %-8s | %-10s |\n", "token", "type", "state");
-	printf("-----------------------------------------\n");
+	printf("----------------------------------------------------\n");
+	printf("| %-13s | %-8s | %-10s | %-10s |\n", "token", "type", "state", "expansion");
+	printf("----------------------------------------------------\n");
 	current = *token_list;
 	while (current)
 	{
 		token = (t_token *)current->content;
-		printf("| %-13s | %-8i | %-10i |\n",
+		printf("| %-13s | %-8i | %-10i | %-10i |\n",
 			token->value,
 			token->type,
-			token->state);
+			token->state,
+			token->expand);
 		current = current->next;
 	}
-	printf("-----------------------------------------\n");
+	printf("-----------------------------------------------------\n");
 }
 
 void	ft_free_list(t_list **list)
 {
 	ft_lstclear(list, free);
-	free(list);
+	if (list)
+		free(list);
 	list = NULL;
 }
 
@@ -46,6 +48,9 @@ void	ft_process_input(char *input, char **my_envp)
 	token_list = ft_create_token_list(trimmed);
 	if (!token_list)
 		return ;
-	print_list(token_list); // delete later
+	ft_print_list(token_list); // delete later
+	//treat expansions and quotes
+	//ft_parse_token_list(token_list, my_envp);
+	//create tree
 	ft_free_list(token_list);
 }
