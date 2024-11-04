cc41cfe0d2881dc60d058ccba3ff3828f1ee18b5
Mariana Oliveira Braga De Morais <mariaoli@c2r1s6.42porto.com>
2024-11-04 19:06:56 +0000

    tokenize.h added bool expansion to t_token

--- includes/tokenize.h
+++ includes/tokenize.h
@@ -9,21 +9,21 @@
 
 typedef enum e_type
 {
-	EXEC,
 	//BUILTIN,
 	PIPE,
-	INFILE,
-	OUTFILE,
 	APPEND,
+	OUTFILE,
 	HEREDOC,
-	EXPORT
+	INFILE,
+	EXPORT, // delete of keep?
+	EXEC
 }	t_type;
 
 typedef enum e_state
 {
-	GENERAL,
 	IN_SQUOTE,
-	IN_DQUOTE
+	IN_DQUOTE,
+	GENERAL
 }	t_state;
 
 typedef struct s_token
@@ -31,6 +31,7 @@ typedef struct s_token
 	char	*value;
 	t_type	type;
 	t_state	state;
+	bool	expand;//
 }	t_token;
 
 typedef struct s_pipe
@@ -48,5 +49,6 @@ char	*ft_charjoin(char *str, char c);
 t_list	**ft_create_token_list(char *s);
 t_type	ft_get_type(char *s);
 t_state	ft_get_state(char *s);
+void	ft_parse_token_list(t_list **token_list, char** my_envp);
 
 #endif
