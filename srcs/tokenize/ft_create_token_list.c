73e8d125c830d66ae236e0a099c51749fcf07692
Mariana Oliveira Braga De Morais <mariaoli@c2r1s6.42porto.com>
2024-11-04 15:35:19 +0000

    ft_create_token_list.c: adjusted it in the hanlde_quotes to save the token correctly

--- srcs/tokenize/ft_create_token_list.c
+++ srcs/tokenize/ft_create_token_list.c
@@ -2,7 +2,6 @@
 
 t_type	ft_get_type(char *s)
 {
-	t_type	type;
 	//int		i;
 	//bool	eligible_export;
 	//bool	export;
@@ -68,32 +67,39 @@ void	ft_add_to_list(char **value, t_list **token_list)
 		new_token->type = ft_get_type(*value);
 		new_token->state = ft_get_state(*value);
 		new_node = ft_lstnew((t_token *)new_token);
+		if (!new_node)
+			return ;
 		ft_lstadd_back(token_list, new_node);
 		free(*value);
 		*value = NULL;
 	}
 }
 
-void	ft_handle_quotes(char **value, char *s, int *i, char quote)
+int	ft_handle_metachar(char **value, char *s, int i, t_list **token_list)
 {
-	*value = ft_charjoin(*value, s[*i]);
-	(*i)++;
-	while (s[*i] != quote)
+	if (*value)
+		ft_add_to_list(value, token_list);
+	*value = ft_charjoin(*value, s[0]);
+	if (ft_strchr(METACHARS, s[1])) // if it's >> or <<
 	{
-		*value = ft_charjoin(value, s[*i]);
-		(*i)++;
+		*value = ft_charjoin(*value, s[1]);
+		i++;
 	}
-	value = ft_charjoin(value, s[*i]);
+	ft_add_to_list(value, token_list);
+	return (i);
 }
 
-void	ft_handle_metachar(char **value, char c, char next, t_list **token_list)
+int	ft_handle_quotes(char **value, char *s, int i, char quote)
 {
-	if (value)
-		ft_add_to_list(value, token_list);
-	value = ft_charjoin(value, c);
-	if (ft_strchr(METACHARS, next)) // if it's >> or <<
-		value = ft_charjoin(value, next);
-	ft_add_to_list(value, token_list);
+	*value = ft_charjoin(*value, s[i]);
+	i++;
+	while (s[i] != quote)
+	{
+		*value = ft_charjoin(*value, s[i]);
+		i++;
+	}
+	*value = ft_charjoin(*value, s[i]);
+	return (i);
 }
 
 void	ft_process_tokens(char *s, t_list **token_list)
@@ -108,9 +114,9 @@ void	ft_process_tokens(char *s, t_list **token_list)
 		if (!ft_isspace(s[i]))
 		{
 			if (ft_strchr(METACHARS, s[i]))
-				ft_handle_metachar(&value, s[i], s[i + 1], token_list);
+				i = ft_handle_metachar(&value, &s[i], i, token_list);
 			else if (s[i] == SQUOTE || s[i] == DQUOTE)
-				ft_handle_quotes(&value, &s[i], &i, s[i]);
+				i = ft_handle_quotes(&value, s, i, s[i]);
 			else
 				value = ft_charjoin(value, s[i]);
 		}
@@ -128,6 +134,7 @@ t_list	**ft_create_token_list(char *s)
 	token_list = (t_list **)malloc(sizeof(t_list **));
 	if (!token_list)
 		return (NULL); // ft_error_handler();
+	*token_list = NULL;
 	ft_process_tokens(s, token_list);
 	return (free(s), token_list);
 }
