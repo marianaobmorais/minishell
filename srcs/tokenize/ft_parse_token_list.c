22a8df6d141f42bffb0b7d97388ff3ef2b869499
Mariana <mariana.obm@gmail.com>
2024-11-05 01:55:05 +0000

    Need to treat cases such as: marianamorais'marianamorais'

--- srcs/tokenize/ft_parse_token_list.c
+++ srcs/tokenize/ft_parse_token_list.c
@@ -1,74 +1,113 @@
 #include "../../includes/minishell.h"
 
-/* char	*ft_expand(char *s, char **my_envp)
+char	*ft_expand(char *s, int *i, char **my_envp)
 {
-	char	*word;
+	char	*expansion;
 	char	*env;
 	char	*env_equal;
 	int		len;
-	int		i;
+	int		j;
 
-	i = 0;
-	if (s[i] == '?') // to do
-	{
-		printf("exit code goes here\n");
-	}
-	while (ft_isalpha(s[i]))
-		i++;
-	env = ft_substr(s, 0, i);
+	len = 0;
+	while (ft_isalpha(s[len]) || ft_isdigit(s[len]) || s[len] == '_')
+		len++;
+	env = ft_substr(s, 0, len);
 	env_equal = ft_strjoin(env, "=");
-	len = ft_strlen(env_equal);
-	//printf("env = %s, len = %d\n", env_equal, len);//
-	i = 0;
-	while (my_envp[i])
+	j = 0;
+	while (my_envp[j])
 	{
-		if (ft_strncmp(env_equal, my_envp[i], len) == 0)
+		if (ft_strncmp(env_equal, my_envp[j], (len + 1)) == 0)
 		{
-			word = ft_strdup(my_envp[i] + len);
-			return (free(env_equal), word);
+			expansion = ft_strdup(my_envp[j] + (len + 1));
+			(*i) += len; // update index past env name
+			return (free(env_equal), expansion);
 		}
-		i++;
+		j++;
 	}
+	(*i) += len; // update index past env name
 	return (free(env_equal), ft_strdup("\0"));
-} */
+}
 
-/* if (s[i] == '$')
+char	*ft_get_exit_code(int *i)
 {
-	i++;
-	expansion = ft_expand_env(&s[i], my_envp);
-	tmp = ft_strjoin(word, expansion);
-	free(word);
-	word = tmp;
-	while (ft_isalpha(s[i]))
-		i++;
-} */
+	(*i)++;
+	return (ft_strdup("EXIT_CODE")); // to do
+}
 
-char	*ft_expand(char *s, char **envp)
+/* void	ft_handle_expansion(t_token **token, char **my_envp)
 {
-	char	new_value;
+	char	*expansion;
+	char	*new_value;
+	char	*tmp;
 	int		i;
 
 	new_value = NULL;
 	i = 0;
-	while (s[i])
+	while ((*token)->value[i])
 	{
-		if (s[i] == '$')
+		if ((*token)->value[i] == '$' && ((ft_isalpha((*token)->value[i + 1]) || (*token)->value[i + 1] == '?' || (*token)->value[i + 1] == '_')))
 		{
 			i++;
-			if (s[i] == '?') // to do
-				printf("exit code goes here\n");
+			if ((*token)->value[i] == '?')
+			{
+				expansion = ft_get_exit_code(&i);
+				tmp = ft_strjoin(new_value, expansion);
+				free(new_value);
+				free(expansion);
+				new_value = tmp;
+			}
 			else
-				while ()
+			{
+				expansion = ft_expand(&(*token)->value[i], &i, my_envp);
+				tmp = ft_strjoin(new_value, expansion);
+				free(new_value);
+				free(expansion);
+				new_value = tmp;
+			}
 		}
-		new_value = ft_charjoin(new_value, s[i]);
-		i++;
+		else if ((*token)->value[i])
+		{
+			if ((*token)->value[i] == SQUOTE)
+			{
+				new_value = ft_charjoin(new_value, (*token)->value[i++]);
+				while ((*token)->value[i] != SQUOTE)
+					new_value = ft_charjoin(new_value, (*token)->value[i++]);
+			}
+			if ((*token)->value[i])
+				new_value = ft_charjoin(new_value, (*token)->value[i++]);
+		}
 	}
-	free(s);
-	return (new_value);
-}
+	free((*token)->value);
+	(*token)->value = new_value;
+} */
+
+// void	ft_handle_expansion(char **new_value, char *s, int *i, char **my_envp)
+// {
+// 	char	*expansion;
+// 	char	*tmp;
+// 	(*i)++;
+// 	if (s[(*i)] == '?')
+// 	{
+// 		expansion = ft_get_exit_code(i);
+// 		tmp = ft_strjoin(*new_value, expansion);
+// 		free(*new_value);
+// 		free(expansion);
+// 		new_value = &tmp;
+// 	}
+// 	else
+// 	{
+// 		expansion = ft_expand(&s[(*i)], i, my_envp);
+// 		tmp = ft_strjoin(*new_value, expansion);
+// 		free(*new_value);
+// 		free(expansion);
+// 		new_value = &tmp;
+// 	}
+// }
 
 void	ft_parse_quote(t_token *token, t_state state)
 {
+	(void)token;
+	(void)state;
 	return ;
 }
 
@@ -76,31 +115,89 @@ void	ft_parse_token_list(t_list **list, char **my_envp)
 {
 	t_list	*current;
 	t_token	*token;
-	char	*expansion;
-	char	*new_value;
-	int		i;
 
-	new_value = NULL;
-	current = list;
+	current = *list;
 	while (current)
 	{
-		if (token->state == EXPAND || token->state == DQUOTE)
+		token = (t_token *)current->content;
+		if (token->expand == true)
 		{
+			//ft_handle_expansion(&token, my_envp);
+			//
+			char	*expansion;
+			char	*new_value;
+			char	*tmp;
+			int		i;
+
+			new_value = NULL;
+			i = 0;
 			while (token->value[i])
 			{
-				if (token->value[i] == '$' && (ft_isalpha(token->value[i + 1]) || token->value[i + 1] == '?'))
+				if (token->value[i] == SQUOTE)
 				{
-					new_value = ft_expand(token->value, my_envp);
-					//free(token->value);
-					//token->value = new_value;
+					new_value = ft_charjoin(new_value, token->value[i++]);
+					while (token->value[i] && token->value[i] != SQUOTE)
+						new_value = ft_charjoin(new_value, token->value[i++]);
+					new_value = ft_charjoin(new_value, token->value[i]);
 				}
-				new_value = ft_charjoin()
-				i++;
+				else if (token->value[i] == DQUOTE)
+				{
+					new_value = ft_charjoin(new_value, token->value[i++]);
+					while (token->value[i] && token->value[i] != DQUOTE)
+					{
+						if (token->value[i] == '$' && ((ft_isalpha(token->value[i + 1]) || token->value[i + 1] == '?' || token->value[i + 1] == '_')))
+						{
+							//ft_handle_expansion(&new_value, &token->value[i], &i, my_envp);
+							i++;
+							if (token->value[i] == '?')
+							{
+								expansion = ft_get_exit_code(&i);
+								tmp = ft_strjoin(new_value, expansion);
+								free(new_value);
+								free(expansion);
+								new_value = tmp;
+							}
+							else
+							{
+								expansion = ft_expand(&token->value[i], &i, my_envp);
+								tmp = ft_strjoin(new_value, expansion);
+								free(new_value);
+								free(expansion);
+								new_value = tmp;
+							}
+						}
+						else
+							new_value = ft_charjoin(new_value, token->value[i++]);
+					}
+				}
+				else if (token->value[i] == '$' && ((ft_isalpha(token->value[i + 1]) || token->value[i + 1] == '?' || token->value[i + 1] == '_')))
+				{
+					//ft_handle_expansion(&new_value, &token->value[i], &i, my_envp);
+					i++;
+					if (token->value[i] == '?')
+					{
+						expansion = ft_get_exit_code(&i);
+						tmp = ft_strjoin(new_value, expansion);
+						free(new_value);
+						free(expansion);
+						new_value = tmp;
+					}
+					else
+					{
+						expansion = ft_expand(&token->value[i], &i, my_envp);
+						tmp = ft_strjoin(new_value, expansion);
+						free(new_value);
+						free(expansion);
+						new_value = tmp;
+					}
+				}
+				else if (token->value[i] && (token->value[i] != DQUOTE))
+					new_value = ft_charjoin(new_value, token->value[i++]);
 			}
+			free(token->value);
+			token->value = new_value;
+			//
 		}
-		//else
-		//	ft_parse_quote(token, token->state);
-
 		current = current->next;
 	}
 }
