08b12aa6cb3b9c78080645806125296c80b30b04
Mariana <mariana.obm@gmail.com>
2024-11-06 22:29:06 +0000

    moved ft_expand_tokens() into ft_process_token_list.c

--- srcs/tokenize/ft_process_token_list.c
+++ srcs/tokenize/ft_process_token_list.c
@@ -23,6 +23,28 @@ void	ft_remove_quotes(t_token *token)
 	return ;
 }
 
+void	ft_expand_tokens(t_token *token, char **my_envp)
+{
+	char	*new_value;
+	int		i;
+
+	new_value = NULL;
+	i = 0;
+	while (token->value[i])
+	{
+		if (token->value[i] == SQUOTE)
+			ft_handle_squotes(&new_value, token->value, &i);
+		else if (token->value[i] == DQUOTE)
+			ft_handle_dquotes(&new_value, token->value, &i, my_envp);
+		else if (token->value[i] == '$' && (ft_isalpha(token->value[i + 1]) || token->value[i + 1] == '?' || token->value[i + 1] == '_'))
+			ft_handle_expansion(&new_value, token->value, &i, my_envp);//
+		else if (token->value[i] && token->value[i] != DQUOTE && token->value[i] != SQUOTE)
+			new_value = ft_charjoin(new_value, token->value[i++]);
+	}
+	free(token->value);
+	token->value = new_value;
+}
+
 void	ft_process_token_list(t_list **list, char **my_envp)
 {
 	t_list	*current;
