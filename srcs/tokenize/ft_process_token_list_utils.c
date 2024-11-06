48bbae8fad507d612d7ce62c89ef56d8e9222a44
Mariana <mariana.obm@gmail.com>
2024-11-06 22:28:02 +0000

    Renamed ft_expand_tokens.c to ft_process_token_list_utils.c

--- srcs/tokenize/ft_process_token_list_utils.c
+++ srcs/tokenize/ft_process_token_list_utils.c
@@ -72,24 +72,3 @@ void	ft_handle_dquotes(char **new_value, char *value, int *i, char **my_envp)
 		*new_value = ft_charjoin(*new_value, value[(*i)++]);
 }
 
-void	ft_expand_tokens(t_token *token, char **my_envp)
-{
-	char	*new_value;
-	int		i;
-
-	new_value = NULL;
-	i = 0;
-	while (token->value[i])
-	{
-		if (token->value[i] == SQUOTE)
-			ft_handle_squotes(&new_value, token->value, &i);
-		else if (token->value[i] == DQUOTE)
-			ft_handle_dquotes(&new_value, token->value, &i, my_envp);
-		else if (token->value[i] == '$' && (ft_isalpha(token->value[i + 1]) || token->value[i + 1] == '?' || token->value[i + 1] == '_'))
-			ft_handle_expansion(&new_value, token->value, &i, my_envp);//
-		else if (token->value[i] && token->value[i] != DQUOTE && token->value[i] != SQUOTE)
-			new_value = ft_charjoin(new_value, token->value[i++]);
-	}
-	free(token->value);
-	token->value = new_value;
-}
