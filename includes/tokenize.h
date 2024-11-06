c5fa33599d337abf321bd78a837c88631078ccdd
Mariana <mariana.obm@gmail.com>
2024-11-06 22:42:31 +0000

    Organized prototypes per file

--- includes/tokenize.h
+++ includes/tokenize.h
@@ -40,23 +40,34 @@ typedef struct s_pipe
 	char	*right;
 }	t_pipe;
 
+//ft_next_quote.c
+int		ft_next_quote(char *s, int i, char c);
+
+//ft_charjoin.c
+char	*ft_charjoin(char *str, char c);
+
 //ft_check_syntax.c
 int		ft_check_syntax(char *s);
 int		ft_isspace(int c);
 
-int		ft_next_quote(char *s, int i, char c);
-void	ft_process_input(char *input, char **my_envp);
-char	*ft_charjoin(char *str, char c);
+//ft_create_token_list.c
 t_list	**ft_create_token_list(char *s);
+//ft_create_token_list_utils.c
 void	ft_add_to_token_list(char **value, t_list **token_list);
-void	ft_process_token_list(t_list **token_list, char** my_envp);
 
+//ft_process_input.c
+void	ft_process_input(char *input, char **my_envp);
+
+//ft_process_token_list.c
+void	ft_process_token_list(t_list **token_list, char** my_envp);
 //ft_process_token_list_utils.c
 void	ft_handle_dquotes(char **new_value, char *value, int *i, char **my_envp);
 void	ft_handle_squotes(char **new_value, char *value, int *i);
 void	ft_handle_expansion(char **new_value, char *value, int *i, char **my_envp);
 char	*ft_get_exit_code(int *i);
 char	*ft_expand_env(char *s, int *i, char **my_envp);
+
+void	*ft_create_tree(token_list);
 
 
 #endif //TOKENIZE_H
