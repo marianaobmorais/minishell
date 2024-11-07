909fdcaac8c3a29b495543b6c39e4e40347f1b13
Mariana <mariana.obm@gmail.com>
2024-11-07 12:14:10 +0000

    Added ft_create_tree.c to Makefile and fixed parameter type

--- srcs/tokenize/ft_create_tree.c
+++ srcs/tokenize/ft_create_tree.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-void	*ft_create_tree(t_token **token_list) //o que retornar? atualizar no header
+void	*ft_create_tree(t_list **token_list) //o que retornar? atualizar no header
 {
 	(void)token_list;
 	return (NULL);
