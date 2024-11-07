85a1d08cf346c9ca661b4eb5c32d7a5b622036fc
Mariana <mariana.obm@gmail.com>
2024-11-07 12:27:29 +0000

    Added @brief descriptions

--- srcs/ft_free_utils.c
+++ srcs/ft_free_utils.c
@@ -16,6 +16,15 @@ void	ft_free_vector(char **vector)
 	free(vector);
 }
 
+/**
+ * @brief Frees all elements of a linked list and the list itself.
+ *
+ * This function iterates through the linked list, freeing each node's content
+ * and the node itself using the `ft_lstclear` function. After clearing the list,
+ * it frees the list pointer and sets it to NULL to prevent dangling pointers.
+ *
+ * @param list A pointer to the linked list to be freed.
+ */
 void	ft_free_list(t_list **list)
 {
 	ft_lstclear(list, free);
