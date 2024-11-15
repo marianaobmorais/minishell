a8fb105484596dcd3b8fcc5163426c468d793335
Mariana <mariana.obm@gmail.com>
2024-11-15 15:25:38 +0000

    ft_build_tree working properly so far. Need to refactor it to norm

--- srcs/tokenize/ft_build_tree.c
+++ srcs/tokenize/ft_build_tree.c
@@ -44,7 +44,7 @@ char	**ft_get_args(t_list **list)
 			args = ft_add_to_vector(args, token->value);
 		else if (token->type == APPEND || token->type == OUTFILE || token->type == HEREDOC || token->type == INFILE)
 		{
-			if (current->next && ((t_token *)current->next->content)->type != PIPE)
+			if (current->next && ((t_token *)current->next->content)->type != PIPE) //the check current->next is redundant
 				current = current->next;
 		}
 		else if (token->type == PIPE)
@@ -61,9 +61,11 @@ bool	ft_find_next_pipe(t_list **list)
 	while (*list)
 	{
 		token = (t_token *)(*list)->content;
+		//printf("find_next_pipe loop: Current token: %s\n", token->value); // Debug
 		if (token->type == PIPE)
 		{
 			*list = (*list)->next; // update list to token after pipe '|' before returning
+			//printf("pipe: Found PIPE, updated list to: %s\n", (*list) ? ((t_token *)(*list)->content)->value : "NULL"); // debug
 			return (true);
 		}
 		*list = (*list)->next;
@@ -78,10 +80,17 @@ bool	ft_find_next_redir(t_list **list)
 	while (*list)
 	{
 		token = (t_token *)(*list)->content;
+		//printf("find_next_redir loop: Current token: %s\n", token->value); // Debug
 		if (token->type == PIPE)
+		{
+			//printf("find_next_redir loop: found pipe -> break\n"); //debug
 			break ;
+		}
 		if (token->type == OUTFILE || token->type == INFILE || token->type == APPEND || token->type == HEREDOC)
+		{
+			//printf("redir: Found redir, updated list to: %s\n", (*list) ? ((t_token *)(*list)->content)->value : "NULL"); // debug
 			return (true);
+		}
 		*list = (*list)->next;
 	}
 	return (false);
@@ -94,10 +103,17 @@ bool	ft_find_next_exec(t_list **list)
 	while (*list)
 	{
 		token = (t_token *)(*list)->content;
+		//printf("find_next_exec loop: Current token: %s\n", token->value); // Debug
 		if (token->type == PIPE)
+		{
+			//printf("find_next_exec loop: found pipe -> break\n"); //debug
 			break ;
+		}
 		if (token->type == EXEC)
+		{
+			//printf("exec: Found exec: %s\n", (*list) ? ((t_token *)(*list)->content)->value : "NULL"); // debug
 			return (true);
+		}
 		*list = (*list)->next;
 	}
 	return (false);
@@ -112,6 +128,7 @@ void	*ft_build_branch(t_list **list, t_exec *exec)
 	token = (*list)->content;
 	if (!exec && (token->type == EXEC || token->type == EXPORT || token->type == EXPORT_AP))
 	{
+		//printf("ft_build_branch: exec\n"); //debug
 		exec = (t_exec *)malloc(sizeof(t_exec));
 		if (!exec)
 			return (NULL); //ft_error_hanlder(); malloc failed
@@ -122,18 +139,28 @@ void	*ft_build_branch(t_list **list, t_exec *exec)
 		while (*list && (token->type == EXEC || token->type == EXPORT || token->type == EXPORT_AP)) // looking for pipe '|', redirs or NULL
 		{
 			token = (*list)->content;
+			//printf("exec loop: Current token: %s\n", token->value); // Debug
 			if (!(token->type == EXEC || token->type == EXPORT || token->type == EXPORT_AP))
 				break ;
 			*list = (*list)->next;
 		}
 		if (!list || !*list)
+		{
+			//printf("exec branch: found null, returning t_exec to pipe->left\n"); //debug
 			return ((void *)exec);
+		}
 		if (token->type == PIPE)
+		{
+			//printf("exec branch: found a pipe\n"); //debug
 			return ((void *)exec);
+		}
+		//else
+		//	printf("exec branch: Found redir: %s\n", (*list) ? ((t_token *)(*list)->content)->value : "NULL"); // debug
 	}
 	//if there are redirs
 	if (token->type == OUTFILE || token->type == INFILE || token->type == APPEND || token->type == HEREDOC)
 	{
+		//printf("ft_build_branch: redir\n"); //debug
 		redir = (t_redir *)malloc(sizeof(t_redir));
 		if (!redir)
 			return (NULL); //ft_error_hanlder(); malloc failed
@@ -145,9 +172,14 @@ void	*ft_build_branch(t_list **list, t_exec *exec)
 			//	printf("how to execute >|?\n");//
 			//else
 			redir->target = (t_token *)(*list)->content;
+			//printf("redir branch: target: %s\n", ((t_token *)(*list)->content)->value); //debug
 		}
 		if (!(*list)->next || ((t_token *)(*list)->next->content == PIPE)) //check whether next is not NULL or PIPE
+		{
+			if (exec)
+				redir->next = exec;
 			return ((void *)redir);
+		}
 		*list = (*list)->next; // move up to next node
 		list_next = *list;
 		if (!exec && ((t_token *)(*list)->content)->type == EXEC)
@@ -157,10 +189,17 @@ void	*ft_build_branch(t_list **list, t_exec *exec)
 		else
 		{
 			if (exec)
+			{
+				//printf("redir branch: next is exec\n"); //debug
 				redir->next = (void *)exec;
+			}
 			else
+			{
+				//printf("redir branch: next is null\n"); //debug
 				redir->next = NULL;
+			}
 		}
+		//printf("redir branch: returning t_redir to pipe->left\n"); //debug
 		return ((void *)redir);
 	}
 	return (NULL);
@@ -182,6 +221,7 @@ void	*ft_build_tree(t_list **list)
 			token = (*list)->content;
 		}
 	}
+	//start root with pipe struct, regardless of the presence of pipes
 	pipe = (t_pipe *)malloc(sizeof(t_pipe));
 	if (!pipe)
 		return (NULL); //ft_error_hanlder(); malloc failed
@@ -191,8 +231,12 @@ void	*ft_build_tree(t_list **list)
 		return (NULL);
 	if (!list || !*list)
 		return ((void *)pipe);
+	//printf("tree: checking content for right pipe: %s\n", ((t_token *)(*list)->content)->value); //debug
 	if (ft_find_next_pipe(list)) // check is there is pipe and if true, update list_to_the_right
+	{
+		//printf("tree: calling ft_build_tree\n"); //debug
 		pipe->right = ft_build_tree(list);
+	}
 	else
 		pipe->right = NULL;
 	return ((void *)pipe);
