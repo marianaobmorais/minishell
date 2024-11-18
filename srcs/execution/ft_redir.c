c322b7ae50a317bcd411b3578d061e80c6c69a4c
Jose Junior <jose_evilasio@outlook.com>
2024-11-18 15:11:38 +0000

    added redir infile outfile

--- srcs/execution/ft_redir.c
+++ srcs/execution/ft_redir.c
@@ -1,12 +1,37 @@
 #include "../../includes/minishell.h"
 
+int	ft_open(int type, char *pathname, int mode)
+{
+	int	fd;
+
+	if (type == INFILE)
+	{
+		if (access(pathname, F_OK) == -1 || access(pathname, R_OK) == -1)
+		{
+			ft_error_handler();
+			exit (1); // tratar
+		}
+	}
+	fd = open(pathname, mode, 0644);
+	if (fd == -1)
+	{
+		ft_error_handler();
+		exit (1); // tratar
+	}
+	return (fd);
+}
+
 int	ft_redir(t_redir *node, char **my_envp)
 {
-	(void)node;
-	if (node->type == OUTFILE)
-		printf("outfile\n");
-	else if (node->type == INFILE)
-		printf("infile\n");
+	int	fd;
+
+	if (node->type == OUTFILE || node->type == INFILE || node->type == APPEND)
+	{
+		fd = ft_open(node->type, node->target->value, node->mode);
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		return (1);
+	}
 	else if (node->type == HEREDOC)
 	{
 		heredoc_fd(node->target->value, my_envp, node->target->state);
