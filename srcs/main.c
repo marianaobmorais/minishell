d5967867a420ee42f22f6260c6793d02218bd9ec
Jose Junior <jose_evilasio@outlook.com>
2024-11-19 22:16:23 +0000

    fixed error handler msg

--- srcs/main.c
+++ srcs/main.c
@@ -8,17 +8,10 @@ int	main(int argc, char **argv, char **envp)
 
 	error_msg = "arguments are not accepted when initializing the program";
 	if (argc != 1)
-	{
-		perror("");
-		return (ft_stderror(error_msg), 0);
-	}
+		return (ft_stderror(FALSE, error_msg), 1);
 	my_envp = ft_get_my_envp(envp);
 	if (!my_envp)
-	{
-		ft_stderror(""); //corrigir para buscar erro do stderr
-		ft_error_handler();
-		return (1); //conferir numero de saida
-	}
+		return (ft_stderror(TRUE, ""), 1);
 	ft_cli(&my_envp);
 	ft_free_vector(my_envp);
 	return (0);
