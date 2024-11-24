151fdf280a3e1f7f4b54f453faea421fba0290b2
Mariana <mariana.obm@gmail.com>
2024-11-24 14:42:11 +0000

    Renamed ft_free_node to ft_free_content

--- srcs/ft_free_utils.c
+++ srcs/ft_free_utils.c
@@ -33,7 +33,7 @@ void	ft_free_vector(char **vector)
  * 
  * @param node Pointer to the list node to be freed.
  */
-void	ft_free_node(t_list *node)
+void	ft_free_content(t_list *node)
 {
 	t_token	*token;
 
@@ -62,20 +62,19 @@ void	ft_free_node(t_list *node)
  */
 void	ft_free_list(t_list **list)
 {
-	t_list	*current;
+	t_list	*current; //update brief
 	t_list	*node;
 
-	if (!list)
+	if (!list || !*list)
 		return ;
 	while (*list)
 	{
 		current = (*list)->next;
 		node = *list;
-		ft_free_node(node);
+		ft_free_content(node);
 		node = NULL;
 		*list = current;
 	}	
 	free(list);
 	list = NULL;
 }
-
