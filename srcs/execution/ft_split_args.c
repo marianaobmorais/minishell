37fdc9e3776a9d78ca6f836c9cc9dfff0cf235ab
Jose Junior <jose_evilasio@outlook.com>
2024-11-27 21:02:54 +0000

    added ft split arg

---
+++ srcs/execution/ft_split_args.c
@@ -0,0 +1,114 @@
+#include "../../includes/minishell.h"
+
+static int	ft_count_words(char *s, char c)
+{
+	bool	in_word;
+	int		count;
+	int		i;
+
+	in_word = false;
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && in_word == false)
+		{
+			in_word = true;
+			count++;
+		}
+		if (s[i] == 39 || s[i] == 34)
+		{
+			i = ft_find_next_quote(s, i, s[i]);
+			if (i == -1)
+				return (-1);
+		}
+		if (s[i] == c)
+			in_word = false;
+		i++;
+	}
+	return (count);
+}
+
+int	ft_offset_str(char *s, char c)
+{
+	int		len;
+
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+	{
+		if (s[len] == 39) 
+			len = ft_find_next_quote(s, len, 39);
+		else if (s[len] == 34)
+			len = ft_find_next_quote(s, len, 34);
+		if (s[len] != c)
+			len++;
+	}
+	return (len);
+}
+
+char	*ft_copy_word(char *s, int len)
+{
+	char	*word;
+	int		i;
+
+	word = NULL;
+	i = 0;
+	while (i < len)
+	{
+		word = ft_charjoin(word, s[i]);
+		i++;
+	}
+	return (word);
+}
+
+static void	ft_clean(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != NULL)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+char	**ft_split_argv(char *s, char c)
+{
+	char	**res;
+	int		word_count;
+	int		offset;
+	size_t	i;
+
+	res = NULL;
+	word_count = ft_count_words(s, c);
+	if (word_count == -1) // if the quotes aren't closed
+	{
+		printf("%s: open quotes are not supported\n", PROG_NAME); //ft_error_handler();
+		return (NULL);
+	}
+	res = (char **)malloc(sizeof(char **) * (word_count + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			offset = ft_offset_str(s, c);
+			res[i] = ft_copy_word(s, offset);
+			if (res[i] == NULL)
+			{
+				ft_clean(res); //ft_error_handler(); 1 //malloc failed
+				return (NULL);
+			}
+			s += offset;
+			i++;
+		}
+		else
+			s++;
+	}
+	res[i] = NULL;
+	return (res);
+}
