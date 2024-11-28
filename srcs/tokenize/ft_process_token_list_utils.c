12d485154799afe7dffbfebd3a4a90558c1e936e
Mariana <mariana.obm@gmail.com>
2024-11-28 11:45:33 +0000

    ft_process_token_list_utils.c's brief is up to date

--- srcs/tokenize/ft_process_token_list_utils.c
+++ srcs/tokenize/ft_process_token_list_utils.c
@@ -3,13 +3,14 @@
 /**
  * @brief Expands an environment variable found in a string.
  * 
- * This function extracts an environment variable name from the input string and searches 
- * for its value in the environment variable array. If found, it returns the corresponding 
- * value; otherwise, it returns an empty string. It updates the index to skip past the 
- * environment variable name.
+ * This function extracts an environment variable name from the input string
+ * and searches for its value in the environment variable array. If found, it
+ * returns the corresponding value; otherwise, it returns an empty string. It
+ * updates the index to skip past the environment variable name.
  * 
  * @param s Pointer to the current position in the input string.
- * @param i Pointer to the index, which is updated to skip past the variable name.
+ * @param i Pointer to the index, which is updated to skip past the variable
+ *          name.
  * @param my_envp Array of environment variables.
  * @return The expanded environment variable value or an empty string.
  */
@@ -32,22 +33,23 @@ char	*ft_expand_env(char *s, int *i, char **my_envp)
 		if (ft_strncmp(env_equal, my_envp[j], (len + 1)) == 0)
 		{
 			expansion = ft_strdup(my_envp[j] + (len + 1));
-			(*i) += len; // update index past env name
+			(*i) += len;
 			return (free(env_equal), free(env), expansion);
 		}
 		j++;
 	}
-	(*i) += len; // update index past env name
-	return (free(env_equal), free(env), ft_strdup("\0")); // this is not an error
+	(*i) += len;
+	return (free(env_equal), free(env), ft_strdup("\0"));
 }
 
 /**
  * @brief Retrieves the exit code as a string.
  * 
- * This function retrieves the last process's exit code. It updates the index 
+ * This function retrieves the last process's exit code. It updates the index
  * and returns the exit_code in string format.
  * 
- * @param i Pointer to the index, incremented to skip past the special character.
+ * @param i Pointer to the index, incremented to skip past the special
+ *        character.
  * @return Exit code value in string format.
  */
 char	*ft_get_exit_code(int *i)
@@ -64,9 +66,10 @@ char	*ft_get_exit_code(int *i)
 /**
  * @brief Handles environment variable expansion in a string.
  * 
- * This function manages the expansion of `$`-prefixed variables in the string. If the `$` 
- * is followed by a `?`, it expands to the exit code. Otherwise, it expands to the 
- * environment variable value if available, updating `new_value` with the expanded result.
+ * This function manages the expansion of `$`-prefixed variables in the string.
+ * If the `$` is followed by a `?`, it expands to the exit code. Otherwise, it
+ * expands to the environment variable value if available, updating `new_value`
+ * with the expanded result.
  * 
  * @param new_value Pointer to the string being built with expansions.
  * @param value Original input string.
@@ -78,7 +81,7 @@ void	ft_handle_expansion(char **new_value, char *value, int *i, char **my_envp)
 	char	*expansion;
 	char	*tmp;
 
-	(*i)++; // skip $
+	(*i)++;
 	if (value[*i] == '?')
 		expansion = ft_get_exit_code(i);
 	else
@@ -92,8 +95,8 @@ void	ft_handle_expansion(char **new_value, char *value, int *i, char **my_envp)
 /**
  * @brief Processes single-quoted text in a string.
  * 
- * This function appends characters within single quotes to `new_value`, preserving the 
- * quotes, and updates the index to skip past the quoted section.
+ * This function appends characters within single quotes to `new_value`,
+ * preserving the quotes,and updates the index to skip past the quoted section.
  * 
  * @param new_value Pointer to the string being built.
  * @param value Original input string.
@@ -110,9 +113,9 @@ void	ft_handle_squotes(char **new_value, char *value, int *i)
 /**
  * @brief Processes double-quoted text with potential variable expansion.
  * 
- * This function appends characters within double quotes to `new_value`, performing 
- * environment variable expansion for `$` symbols where applicable. It updates the index 
- * to skip past the quoted section.
+ * This function appends characters within double quotes to `new_value`,
+ * performing environment variable expansion for `$` symbols where applicable.
+ * It updates the index to skip past the quoted section.
  * 
  * @param new_value Pointer to the string being built with expansions.
  * @param value Original input string.
