465bef51e86fa719ad039ae2077871af77b7b30e
Mariana <mariana.obm@gmail.com>
2024-11-28 12:12:42 +0000

    Updated brief in ft_charjoin.c

--- srcs/tokenize/ft_charjoin.c
+++ srcs/tokenize/ft_charjoin.c
@@ -1,15 +1,18 @@
 #include "../../includes/minishell.h"
 
 /**
- * @brief Concatenates a character to the end of a string and returns the new string.
+ * @brief Concatenates a character to the end of a string and returns the new
+ *        string.
  *
- * This function allocates memory for a new string, copies the original string into it,
- * appends the specified character to the end, and then returns the newly created string.
- * The original string is freed after the operation. The new string is null-terminated.
+ * This function allocates memory for a new string, copies the original string
+ * into it, appends the specified character to the end, and then returns the
+ * newly created string. The original string is freed after the operation. The
+ * new string is null-terminated.
  *
  * @param str The original string to which the character will be appended.
  * @param c The character to append to the string.
- * @return A new string with the character appended, or NULL if memory allocation fails.
+ * @return A new string with the character appended, or NULL if memory
+ *         allocation fails.
  */
 char	*ft_charjoin(char *str, char c)
 {
