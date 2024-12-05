d50884ea750c4cd6887b86ab89e383325a70cd44
Jose Junior <jose_evilasio@outlook.com>
2024-12-05 16:11:58 +0000

    fixed handler error

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -1,11 +1,57 @@
 #include "../../includes/minishell.h"
 
+/**
+ * @brief Prints "exit" message if the shell is running in an interactive mode.
+ *
+ * Outputs "exit" to the standard error stream if the stdin is attached 
+ * to a terminal (interactive mode). Does nothing in non-interactive mode.
+ */
+static void	print_exit(void)
+{
+	if (isatty(STDIN_FILENO))
+		ft_putendl_fd("exit", 2);
+}
+
+/**
+ * @brief Checks if a string argument is not a valid numeric value.
+ *
+ * Determines whether the given string is not numeric. Accepts optional '+' or 
+ * '-' prefixes followed by digits. Logs an error if the argument is invalid.
+ *
+ * @param arg The string to validate.
+ *
+ * @return TRUE if the string is not numeric, FALSE otherwise.
+ */
+static int	isnumeric(char *arg)
+{
+	if (arg[0] == '\0' || ((arg[0] == '-' || arg[0] == '+')
+			&& !ft_isdigit(arg[1])))
+	{
+		ft_stderror(FALSE, "exit: %s: numeric argument required", arg);
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+/**
+ * @brief Converts a string argument to a numeric value with validation.
+ *
+ * Validates and converts a string argument to a long integer. If the argument 
+ * is not numeric or exceeds the range of a 32-bit integer, it returns an exit 
+ * status of 2 and logs an error. Handles optional '+' or '-' prefixes.
+ *
+ * @param arg The string argument to convert.
+ * 
+ * @return The converted numeric value on success or triggers an error with 
+ *         an exit status of 2 if validation fails.
+ */
 static long	arg_convert(char *arg)
 {
 	int			i;
 	long long	num;
 
 	i = 0;
+	ft_exit_status(0, TRUE, FALSE);
 	if (arg[i] == '-' || arg[i] == '+')
 		i++;
 	while (arg[i])
@@ -13,7 +59,7 @@ static long	arg_convert(char *arg)
 		if (ft_isdigit(arg[i]) == 0)
 		{
 			ft_stderror(FALSE, "exit: %s: numeric argument required", arg);
-			ft_exit_status(2, TRUE, FALSE);
+			return (ft_exit_status(2, TRUE, FALSE));
 		}
 		i++;
 	}
@@ -21,23 +67,20 @@ static long	arg_convert(char *arg)
 	if (num > INT32_MAX || num < INT32_MIN)
 	{
 		ft_stderror(FALSE, "exit: %s: numeric argument required", arg);
-		ft_exit_status(2, TRUE, FALSE);
+		return (ft_exit_status(2, TRUE, FALSE));
 	}
 	return ((long) num);
 }
 
 /**
- * @brief Exits the program with a specified exit status and displays an exit
- * message.
+ * @brief Handles the exit command
  *
- * This function prints "exit" to the standard error output (file descriptor 2)
- * and then calls `ft_exit_status` to set the exit status. If `exit_` is `TRUE`
- * it terminates the program using the current status.
+ * Processes arguments for the `exit` command, validating them and determining 
+ * the appropriate exit status. Handles invalid numeric arguments, excess 
+ * arguments, or no arguments. Exits with the specified status if valid.
  *
- * @param exit_ If `TRUE`, the function will terminate the program with the 
- * 				current exit status.
- *              If `FALSE`, it only updates the exit status without terminating.
- * @param exit_status Terminate the program with the current exit status.
+ * @param argc The number of arguments passed to the exit command.
+ * @param args The array of arguments, where args[0] is the command name.
  */
 void	ft_exit(int argc, char **args)
 {
@@ -46,26 +89,24 @@ void	ft_exit(int argc, char **args)
 
 	i = 1;
 	exit_status = 0;
-	if (argc > 1)
+	print_exit();
+	if (argc == 1)
+		ft_exit_status(exit_status, TRUE, TRUE);
+	while (args[i])
 	{
-		while (args[i])
+		if (isnumeric(args[i]))
+			ft_exit_status(2, TRUE, TRUE);
+		else if (i == 2)
 		{
-			if (i == 2)
-			{
-				ft_stderror(FALSE, "exit: too many arguments");
-				ft_exit_status(1, TRUE, FALSE);
-			}
-			if (args[i][0] == '\0' || ((args[i][0] == '-'
-				|| args[i][0] == '+') && !ft_isdigit(args[i][1])))
-			{
-				ft_stderror(FALSE, "exit: %s: numeric argument required", args[i]);
-				ft_exit_status(2, TRUE, FALSE);
-			}
-			exit_status = (unsigned char) arg_convert(args[i]);
-			i++;
+			ft_stderror(FALSE, "exit: too many arguments");
+			ft_exit_status(1, TRUE, FALSE);
 		}
+		else
+		{
+			exit_status = (unsigned char) arg_convert(args[i]);
+			if (ft_exit_status(0, FALSE, FALSE))
+				ft_exit_status(exit_status, TRUE, TRUE);
+		}
+		i++;
 	}
-	if (isatty(STDIN_FILENO))
-		ft_putendl_fd("exit", 2);
-	ft_exit_status(exit_status, TRUE, TRUE);
 }
