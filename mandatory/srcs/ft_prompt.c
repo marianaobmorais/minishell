6a1fc0bb54f2f9e0e755d61fc55e7448734c3dfe
Jose Junior <jose_evilasio@outlook.com>
2024-12-16 19:43:05 +0000

    added brief

--- mandatory/srcs/ft_prompt.c
+++ mandatory/srcs/ft_prompt.c
@@ -1,5 +1,15 @@
 #include "../includes/minishell.h"
 
+/**
+ * @brief Constructs and sets the shell prompt based on the current directory.
+ *
+ * This function retrieves the current working directory and constructs the
+ * shell prompt by combining predefined prompt parts with the current path.
+ * The constructed prompt is then assigned to the `prompt` field of the
+ * `t_shell` structure.
+ *
+ * @param sh A pointer to the `t_shell` structure containing the shell state.
+ */
 void	ft_prompt(t_shell *sh)
 {
 	char	*prompt;
