794aa640dc9b89405d069fd7d3418efb443073ac
Mariana Oliveira Braga De Morais <mariaoli@c2r1s8.42porto.com>
2024-12-16 18:54:55 +0000

    Added header. Norminette is ok

--- mandatory/includes/tokenize.h
+++ mandatory/includes/tokenize.h
@@ -139,8 +139,4 @@ void	ft_free_tree(void *root);
 bool	ft_is_token_type(t_token *token, t_type type);
 bool	ft_is_node_type(t_node *node, t_type type);
 
-//debugging
-void	ft_print_list(t_list **token_list);
-void	print_root(void *root, int indent);
-
 #endif //TOKENIZE_H
